d72561bf8a143191352d021ddca38d774424a089
Udit-github <udit96405@gmial.com>
2020-12-13 00:05:10 +0530

    Site Ready for Deploy

--- Apps.js
+++ Apps.js
@@ -1,9 +1,27 @@
 var btnref = document.querySelector("#btn-ref");
 var txtinput= document.querySelector("#txt-area");
+var outputText= document.querySelector("#txt-area-output");
 
+var url="https://api.funtranslations.com/translate/minion.json";
+
+
+function translatedUrl(text){
+    return url+"?"+"text= "+text;
+}
 btnref.addEventListener("click",function clickHandler(){
-    console.log("It worked");
-    console.log("input",txtinput.value);
+    // console.log("It worked");
+    // console.log("input",txtinput.value);
+
+    var inputFinal = txtinput.value;
+    
+
+    fetch(translatedUrl(inputFinal))   // or we can write like fetch(translatedUrl(inputFinal))
+    .then(response => response.json())
+    .then(json => { //console.log(json)
+         var translatedText=json.contents.translated;
+         outputText.innerText= translatedText;
+    }) 
+
     
 })
 
