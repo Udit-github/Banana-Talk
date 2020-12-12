c8ce7c9920cffe93a0447bd13d7849c7c877626e
Udit-github <udit96405@gmial.com>
2020-12-12 18:33:50 +0530

    DONE

---
+++ Apps.js
@@ -0,0 +1,10 @@
+var btnref = document.querySelector("#btn-ref");
+var txtinput= document.querySelector("#txt-area");
+
+btnref.addEventListener("click",function clickHandler(){
+    console.log("It worked");
+    console.log("input",txtinput.value);
+    
+})
+
+ 
